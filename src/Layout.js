fe00db1cb6870809bf0f8e08f60f7582a933916c
trishantjain <trishantjain8888@gmail.com>
2023-10-09 21:37:59 +0530

    Removed Gliter effect

--- src/Layout.js
+++ src/Layout.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import Confetti from "react-confetti";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
@@ -7,14 +6,6 @@ import PreLoader from "./components/PreLoader";
 
 export default function Layout() {
 	const [isLoading, setIsLoading] = useState(true);
-	const [isConfetti, setIsConfetti] = useState(true);
-	const [bodyWidth, setBodyWidth] = useState(document.body.innerWidth);
-	const [bodyHeight, setBodyHeight] = useState(document.body.innerHeight);
-
-	function handleSizeChange() {
-		setBodyWidth(document.body.clintWidth);
-		setBodyHeight(document.body.clintHeight);
-	}
 
 	useEffect(() => {
 		const delay = () => {
@@ -24,28 +15,11 @@ export default function Layout() {
 		};
 		delay();
 	}, []);
-	useEffect(() => {
-		document.body.onresize = () => handleSizeChange();
-		const delay = () => {
-			setTimeout(() => {
-				setIsConfetti(false);
-			}, 9000);
-		};
-		delay();
-	}, []);
+	
 	return isLoading ? (
 		<PreLoader />
 	) : (
 		<body>
-			{isConfetti && (
-				<Confetti
-					width={bodyWidth}
-					height={bodyHeight}
-					numberOfPieces={200}
-					gravity={0.06}
-					wind={0.01}
-				/>
-			)}
 			<NavBar />
 			<main className="pt-20 min-h-[95vh]">
 				<Outlet />
