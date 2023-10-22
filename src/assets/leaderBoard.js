cf59236ae2c1047f8e871049335cefd2f8119b28
trishantjain <trishantjain8888@gmail.com>
2023-10-22 19:18:13 +0530

    Updated LeaderBoard.js

--- src/assets/leaderBoard.js
+++ src/assets/leaderBoard.js
@@ -2,11 +2,131 @@ export const LeaderBoards =
 [
 	{
         "sl": "1",
-        "Name": "Varun Parashar",
+        "Name": "BHUMIKA SARASWAT",
     }, 
     {
         "sl": "2",
-        "Name": "Tanishq Kumar",
+        "Name": "ABHISHEK",
+    },
+    {
+        "sl": "3",
+        "Name": "ABHISHEK",
+    },
+    {
+        "sl": "4",
+        "Name": "LOKESH SINGH",
+    },
+    {
+        "sl": "5",
+        "Name": "VARUN PARASHAR",
+    },
+    {
+        "sl": "6",
+        "Name": "TANISHQ KUMAR",
+    },
+    {
+        "sl": "7",
+        "Name": "GAURAV BANSAL",
+    },
+    {
+        "sl": "8",
+        "Name": "ISHA RANI",
+    },
+    {
+        "sl": "9",
+        "Name": "YASHIKA",
+    },
+    {
+        "sl": "10",
+        "Name": "SHEETAL",
+    },
+    {
+        "sl": "11",
+        "Name": "PRITAM KUMAR",
+    },
+    {
+        "sl": "12",
+        "Name": "DIYA GUPTA",
+    },
+    {
+        "sl": "13",
+        "Name": "DEEPA GUPTA",
+    },
+    {
+        "sl": "14",
+        "Name": "ANIKET SHAHI",
+    },
+    {
+        "sl": "15",
+        "Name": "TARUN KOHLI",
+    },
+    {
+        "sl": "16",
+        "Name": "SONAKSHI SHARMA",
+    },
+    {
+        "sl": "17",
+        "Name": "RIA CHHABRA",
+    },
+    {
+        "sl": "18",
+        "Name": "NITIKA RANI",
+    },
+    {
+        "sl": "19",
+        "Name": "HIMANSHU",
+    },
+    {
+        "sl": "20",
+        "Name": "SANDESH BORKAR",
+    },
+    {
+        "sl": "21",
+        "Name": "DIKSHANT PRAJAPATI",
+    },
+    {
+        "sl": "22",
+        "Name": "LAKSHAY DALAL",
+    },
+    {
+        "s": "23",
+        "Name": "ADITYA RAJ",
+    },
+    {
+        "s": "24",
+        "Name": "PRAKHAR",
+    },
+    {
+        "s": "25",
+        "Name": "ADITI GARG",
+    },
+    {
+        "s": "26",
+        "Name": "ANANYA",
+    },
+    {
+        "s": "27",
+        "Name": "GUNGUN NAGPAL",
+    },
+    {
+        "s": "28",
+        "Name": "RAVINDER",
+    },
+    {
+        "s": "29",
+        "Name": "TUSHAR",
+    },
+    {
+        "s": "30",
+        "Name": "SHIVENDRA NATH DWIVEDI",
+    },
+    {
+        "s": "31",
+        "Name": "SHIVAM BHATIA",
+    },
+    {
+        "s": "32",
+        "Name": "ARSH GUPTA",
     }
 
 ];
