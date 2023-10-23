c28545126f4ddb988e7b3ad0fadc1a262768ed74
trishantjain <trishantjain8888@gmail.com>
2023-10-23 19:01:21 +0530

    Updated LeaderBoard.js

--- src/assets/leaderBoard.js
+++ src/assets/leaderBoard.js
@@ -14,170 +14,174 @@ export const LeaderBoards =
     },
     {
         "sl": "4",
-        "Name": "LOKESH SINGH",
+        "Name": "PRAGYA PARASHAR",
     },
     {
         "sl": "5",
-        "Name": "VARUN PARASHAR",
+        "Name": "LOKESH SINGH",
     },
     {
         "sl": "6",
-        "Name": "TANISHQ KUMAR",
+        "Name": "VARUN PARASHAR",
     },
     {
         "sl": "7",
-        "Name": "GAURAV BANSAL",
-    },
-    {
-        "sl": "8",
-        "Name": "ISHA RANI",
-    },
-    {
-        "sl": "9",
-        "Name": "YASHIKA",
-    },
-    {
-        "sl": "10",
-        "Name": "SHEETAL",
-    },
-    {
-        "sl": "11",
-        "Name": "PRITAM KUMAR",
-    },
-    {
-        "sl": "12",
-        "Name": "DIYA GUPTA",
-    },
-    {
-        "sl": "13",
-        "Name": "DEEPA GUPTA",
-    },
-    {
-        "sl": "14",
-        "Name": "ANIKET SHAHI",
-    },
-    {
-        "sl": "15",
-        "Name": "TARUN KOHLI",
-    },
-    {
-        "sl": "16",
-        "Name": "SONAKSHI SHARMA",
-    },
-    {
-        "sl": "17",
-        "Name": "RIA CHHABRA",
-    },
-    {
-        "sl": "18",
-        "Name": "NITIKA RANI",
-    },
-    {
-        "sl": "19",
-        "Name": "HIMANSHU",
-    },
-    {
-        "sl": "20",
-        "Name": "SANDESH BORKAR",
-    },
-    {
-        "sl": "21",
-        "Name": "DIKSHANT PRAJAPATI",
-    },
-    {
-        "sl": "22",
-        "Name": "LAKSHAY DALAL",
-    },
-    {
-        "sl": "23",
-        "Name": "ADITYA RAJ",
-    },
-    {
-        "sl": "24",
-        "Name": "PRAKHAR",
-    },
-    {
-        "sl": "25",
-        "Name": "ADITI GARG",
-    },
-    {
-        "sl": "26",
-        "Name": "ANANYA",
-    },
-    {
-        "sl": "27",
-        "Name": "GUNGUN NAGPAL",
-    },
-    {
-        "sl": "28",
-        "Name": "RAVINDER",
-    },
-    {
-        "sl": "29",
-        "Name": "TUSHAR",
-    },
-    {
-        "sl": "30",
-        "Name": "SHIVENDRA NATH DWIVEDI",
-    },
-    {
-        "sl": "31",
-        "Name": "SHIVAM BHATIA",
-    },
-    {
-        "sl": "32",
-        "Name": "ARSH GUPTA",
-    },
-    {
-        "sl": "33",
-        "Name": "ANUSHKA GUPTA",
-    },
-    {
-        "sl": "34",
-        "Name": "MUKUL UPADHYAY",
-    },
-    {
-        "sl": "35",
-        "Name": "AKANSHA NEGI",
-    },
-    {
-        "sl": "36",
-        "Name": "NAINA KHARAYAT",
-    },
-    {
-        "sl": "37",
-        "Name": "HITESH",
-    },
-    {
-        "sl": "38",
-        "Name": "DHRUV SHARMA",
-    },
-    {
-        "sl": "39",
-        "Name": "VISHAL ",
-    },
-    {
-        "sl": "40",
-        "Name": "VARNIT AGARWAL",
-    },
-    {
-        "sl": "41",
-        "Name": "TUSHAR BANGA",
-    },
-    {
-        "sl": "42",
-        "Name": "SAKSHI",
-    },
-    {
-        "sl": "43",
         "Name": "TANISHQ KUMAR",
     },
     {
+        "sl": "8",
+        "Name": "GAURAV BANSAL",
+    },
+    {
+        "sl": "9",
+        "Name": "ISHA RANI",
+    },
+    {
+        "sl": "10",
+        "Name": "YASHIKA",
+    },
+    {
+        "sl": "11",
+        "Name": "SHEETAL",
+    },
+    {
+        "sl": "12",
+        "Name": "PRITAM KUMAR",
+    },
+    {
+        "sl": "13",
+        "Name": "DIYA GUPTA",
+    },
+    {
+        "sl": "14",
+        "Name": "DEEPA GUPTA",
+    },
+    {
+        "sl": "15",
+        "Name": "ANIKET SHAHI",
+    },
+    {
+        "sl": "16",
+        "Name": "TARUN KOHLI",
+    },
+    {
+        "sl": "17",
+        "Name": "SONAKSHI SHARMA",
+    },
+    {
+        "sl": "18",
+        "Name": "RIA CHHABRA",
+    },
+    {
+        "sl": "19",
+        "Name": "NITIKA RANI",
+    },
+    {
+        "sl": "20",
+        "Name": "HIMANSHU",
+    },
+    {
+        "sl": "21",
+        "Name": "SANDESH BORKAR",
+    },
+    {
+        "sl": "22",
+        "Name": "DIKSHANT PRAJAPATI",
+    },
+    {
+        "sl": "23",
+        "Name": "LAKSHAY DALAL",
+    },
+    {
+        "sl": "24",
+        "Name": "ADITYA RAJ",
+    },
+    {
+        "sl": "25",
+        "Name": "PRAKHAR",
+    },
+    {
+        "sl": "26",
+        "Name": "ADITI GARG",
+    },
+    {
+        "sl": "27",
+        "Name": "ANANYA",
+    },
+    {
+        "sl": "28",
+        "Name": "GUNGUN NAGPAL",
+    },
+    {
+        "sl": "29",
+        "Name": "RAVINDER",
+    },
+    {
+        "sl": "30",
+        "Name": "TUSHAR",
+    },
+    {
+        "sl": "31",
+        "Name": "SHIVENDRA NATH DWIVEDI",
+    },
+    {
+        "sl": "32",
+        "Name": "SHIVAM BHATIA",
+    },
+    {
+        "sl": "33",
+        "Name": "ARSH GUPTA",
+    },
+    {
+        "sl": "34",
+        "Name": "ANUSHKA GUPTA",
+    },
+    {
+        "sl": "35",
+        "Name": "MUKUL UPADHYAY",
+    },
+    {
+        "sl": "36",
+        "Name": "AKANSHA NEGI",
+    },
+    {
+        "sl": "37",
+        "Name": "NAINA KHARAYAT",
+    },
+    {
+        "sl": "38",
+        "Name": "HITESH",
+    },
+    {
+        "sl": "39",
+        "Name": "DHRUV SHARMA",
+    },
+    {
+        "sl": "40",
+        "Name": "VISHAL ",
+    },
+    {
+        "sl": "41",
+        "Name": "VARNIT AGARWAL",
+    },
+    {
+        "sl": "42",
+        "Name": "TUSHAR BANGA",
+    },
+    {
+        "sl": "43",
+        "Name": "SAKSHI",
+    },
+    {
         "sl": "44",
-        "Name": "MICHAEL MUTHURAJ",
+        "Name": "TANISHQ KUMAR",
     },
     {
         "sl": "45",
+        "Name": "MICHAEL MUTHURAJ",
+    },
+    {
+        "sl": "46",
         "Name": "BHUVI SAINI",
     },
 
